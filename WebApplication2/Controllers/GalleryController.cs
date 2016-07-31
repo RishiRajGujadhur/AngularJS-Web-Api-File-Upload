c7ec784ea5a2a617e4bf042f5021d7ed375dee79
Rishi Raj Gujadhur <r.r.gujadhur@gmail.com>
2016-07-31 20:48:45 +0400

    File Upload using AngularJS
    AngularJS Web API App

---
+++ WebApplication2/Controllers/GalleryController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication2.Controllers
+{
+    public class GalleryController : ApiController
+    {
+        private FileUploadDemoDbContext Context = new FileUploadDemoDbContext();
+
+        [HttpPost]
+        [Route("api/FileUpload")]
+        public IHttpActionResult Upload()
+        {
+
+            System.Threading.Thread.Sleep(5000);
+            int uploadCount = 0;
+            string sPath = System.Web.Hosting.HostingEnvironment.MapPath("/Gallery/");
+
+            System.Web.HttpFileCollection files = System.Web.HttpContext.Current.Request.Files;
+
+            for (int i = 0; i < files.Count; i++)
+            {
+                System.Web.HttpPostedFile file = files[i];
+            
+            string fileName = new FileInfo(file.FileName).Name;
+
+            if(file.ContentLength >0)
+            {
+
+                Guid id = Guid.NewGuid();
+
+                string modifiedFileName = id.ToString() + "_" + fileName;
+
+                if (!File.Exists(sPath + Path.GetFileName(modifiedFileName)))
+                {
+                    file.SaveAs(sPath + Path.GetFileName(modifiedFileName));
+                    uploadCount++;
+
+                    Context.Galleries.Add(new Gallery() { Id = id, FileName = "/Gallery/" + modifiedFileName, Title = fileName });
+                }
+            }
+            }
+
+            if(uploadCount >0)
+            {
+                Context.SaveChanges();
+                return Ok("Uploaded Successfully");
+
+            }
+            return InternalServerError();
+
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing)
+            {
+                Context.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication2/WEB/App/file-upload.js
@@ -0,0 +1,94 @@
+﻿/// <reference path="C:\Users\GUJADHUR\Desktop\WebApplication2\WebApplication2\Scripts/angular.min.js" />
+angular.module('fupApp', [])
+.directive('ngFiles', ['$parse', function ($parse) {
+
+    function fn_link(scope, element, attrs)
+    {
+        var onChange = $parse(attrs.ngFiles);
+
+        element.on('change', function (event) {
+
+            onChange(scope, { $files: event.target.files });
+        })
+    }
+    return {
+
+        link:fn_link
+    }
+}])
+.controller('fupController', function ($scope,$http) {
+
+    var formdata = new FormData();
+
+    $scope.getTheFiles = function ($files) {
+
+        $scope.imagesrc = [];
+
+        for (var i = 0; i < $files.length; i++)
+        {
+            var reader = new FileReader();
+            reader.fileName = $files[i].name;
+            reader.onload = function (event) {
+                var image = {};
+                image.Name = event.target.fileName;
+                image.Size = (event.total / 1024).toFixed(2);
+                image.Src = event.target.result;
+                $scope.imagesrc.push(image);
+                $scope.$apply();
+
+            }
+            reader.readAsDataURL($files[i]);
+        }
+        angular.forEach($files, function (value, key)
+        {
+            formdata.append(key, value);
+        })
+    }
+    
+    $scope.uploadFiles = function () {
+        var request = {
+
+            method: 'Post',
+            url: '/api/FileUpload',
+            data: formdata,
+            headers: {
+                'Content-Type': undefined
+
+            }
+
+        };
+        $scope.sloadingImage();
+        $http(request).success(function (d) {
+            $scope.data = false;
+            alert(d);
+            $scope.reset();
+            $scope.hloadingImage();
+
+        }).error(function () {
+            
+            alert("failed");
+            $scope.reset();
+            $scope.hloadingImage();
+           
+        })
+
+    }
+
+    $scope.reset = function () {
+        $scope.imagesrc = [];
+        formdata = new FormData();
+        $("#fileControl").val('');
+    }
+
+    $scope.sloadingImage = function () {
+        $("#spinner").show();
+        $("#spinner").css({ display: "normal" });
+    }
+
+    $scope.hloadingImage = function () {
+        $("#spinner").hide();
+        $("#spinner").css({ display: "none" });
+    }
+    
+  
+})
